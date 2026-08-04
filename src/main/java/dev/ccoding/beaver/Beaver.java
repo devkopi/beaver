62fd2ff94f02f54fbf4a565579371ee66aa06276
Santiago Gualdron <manugualdron@gmail.com>
2026-08-03 21:35:39 -0500

    chore: initial commit

---
+++ src/main/java/dev/ccoding/beaver/Beaver.java
@@ -0,0 +1,17 @@
+package dev.ccoding.beaver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Beaver extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
